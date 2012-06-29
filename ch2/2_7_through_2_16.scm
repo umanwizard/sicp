544040f57db43a414755dd907aa0897107db0c5c
Brennan Vincent <brennanv@email.arizona.edu>
2012-06-29 07:23:03 -0700

    Completed some exercises

--- ch2/2_7_through_2_16.scm
+++ ch2/2_7_through_2_16.scm
@@ -28,5 +28,12 @@
       (error "One of the intervals provided to div-interval spans zero!")
       (do-div-interval x y)))
 
-;2.11
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+	(p2 (* (lower-bound x) (upper-bound y)))
+	(p3 (* (upper-bound x) (lower-bound y)))
+	(p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+		   (max p1 p2 p3 p4))))
+
 
