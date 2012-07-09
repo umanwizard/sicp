1970a60b91eff05defe6b76f01ec2903e52f560f
Brennan Vincent <brennanv@email.arizona.edu>
2012-07-09 08:56:42 -0700

    Exercise 2.54

---
+++ ch2/2_54.scm
@@ -0,0 +1,8 @@
+(define (equal? a b)
+  (cond ((and (symbol? a) (symbol? b))
+	 (eq? a b))
+	((and (null? a) (null? b))
+	 #t)
+	((and (list? a) (list? b))
+	 (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))
+	(else #f)))
